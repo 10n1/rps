4553f377dc97fb4bb3954487dde5a0c92d91688d
Omar A Rodriguez <omar@gnlnx.net>
2012-09-11 00:19:06 -0700

    Revert "fully replace GLKMatrix"
    This reverts commit 3002912491f16a9bf1049494f54027a3b799a855.

--- rps/render.c
+++ rps/render.c
@@ -78,6 +78,7 @@ enum {
 };
 
 static GLuint               _character_meshes[256] = {0};
+//static GLKMatrix4           _projectionMatrix[kNumProjectionTypes];
 static float4x4             _projectionMatrix[kNumProjectionTypes];
 static GLuint               _program = 0;
 static GLuint               _meshes[NUM_MESHES] = {0};
