0156b4bd97d0405f031e37354419afd9089b4ea0
Omar A Rodriguez <omar@gnlnx.net>
2012-11-01 23:59:46 -0700

    Fixed the differences in OpenGL ES headers for Android and iOS

--- rps/game.h
+++ rps/game.h
@@ -8,9 +8,18 @@
 #define _game_h__
 
 #include <stdint.h>
+//#include <OpenGLES/ES2/gl.h>
+#ifdef ANDROID
+#include <GLES2/gl2.h>
+#include <GLES2/gl2ext.h>
+#else  // For iOS
 #include <OpenGLES/ES2/gl.h>
+#include <OpenGLES/ES2/glext.h>
+#endif
+
+#include "rps_math.h"
 #include "timer.h"
-#include "ui.h"
+// #include "ui.h"
 
 typedef struct game_t game_t;
 typedef struct player_t player_t;
@@ -53,21 +62,21 @@ struct game_t {
     float           speed;
     player_t        player;
     game_state_t    state;
-    button_t*       pause_button;
-    button_t*       resume_button;
-    button_t*       pause_background;
-    button_t*       quit_button;
-    button_t*       weapon_buttons[kNumWeapons];
-    button_t*       play_button;
+    // button_t*       pause_button;
+    // button_t*       resume_button;
+    // button_t*       pause_background;
+    // button_t*       quit_button;
+    // button_t*       weapon_buttons[kNumWeapons];
+    // button_t*       play_button;
 };
 
 void game_initialize(game_t* game, float width, float height);
 void game_update(game_t* game);
 void game_render(game_t* game);
 void game_shutdown(game_t* game);
-void game_pause(ui_param_t* p);
-void game_resume(ui_param_t* p);
-void game_toggle_pause(ui_param_t* params);
+// void game_pause(ui_param_t* p);
+// void game_resume(ui_param_t* p);
+// void game_toggle_pause(ui_param_t* params);
 
 void game_clear_touch(game_t* game);
 void game_handle_touch(game_t* game, float x, float y);
